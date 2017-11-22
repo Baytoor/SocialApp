7604dd73bb236b2b352c16618eb87748c647ef73
Baytoor <ms.ms.1@bk.ru>
2017-11-23 03:13:45 +0600

    emailAuth
    Email authentication was added and handled few errors

--- SocialApp/Controller/LaunchPageVC.swift
+++ SocialApp/Controller/LaunchPageVC.swift
@@ -15,14 +15,51 @@ import FirebaseAuth
 class LaunchPageVC: UIViewController {
 
     @IBOutlet weak var facebook: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var errorLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         facebook.setImage(#imageLiteral(resourceName: "facebook").maskWithColor(color: UIColor(lightBlue)), for: .highlighted)
+        errorLbl.isHidden = true
     }
 
-    @IBAction func signInPage(_ sender: Any) {
-        self.performSegue(withIdentifier: "SignInVC", sender: self)
+    @IBAction func signInPressed(_ sender: Any) {
+        if let email = emailField.text, let pass = passField.text {
+            if pass.count >= 6 {
+            Auth.auth().signIn(withEmail: email, password: pass, completion: { (user, error) in
+                if error == nil {
+                    print("MSG: User authenticated with firebase using email")
+                } else {
+                    let errCode = AuthErrorCode(rawValue: error!._code)
+                    if errCode == AuthErrorCode.userNotFound{
+                        Auth.auth().createUser(withEmail: email, password: pass, completion: { (user, error) in
+                            if error != nil {
+                                print("MSG: Unable to authenticate with firebase using email")
+                            } else {
+                                print("MSG: New user was created usting email")
+                            }
+                        })
+                    } else if errCode == AuthErrorCode.invalidEmail {
+                        self.errorLbl.isHidden = false
+                        self.errorLbl.text = "Invalid email"
+                        self.errorLbl.textColor = UIColor.red
+                    } else if errCode == AuthErrorCode.wrongPassword {
+                        self.errorLbl.isHidden = false
+                        self.errorLbl.text = "Wrong password"
+                        self.errorLbl.textColor = UIColor.red
+                    }
+                }
+            })
+            } else {
+                errorLbl.isHidden = false
+                errorLbl.text = "Password must have 6 characters"
+                errorLbl.textColor = UIColor.red
+            }
+            
+        }
     }
     
     @IBAction func fbRegisterBtn(_ sender: Any){
